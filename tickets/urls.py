220d97416e61ddeca95f88680d77b62a8e1cfc9c
rukigaki <rukigaki@gmail.com>
2026-07-28 14:22:38 +0300

    Default DRF API structure

---
+++ tickets/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import TicketViewSet
+
+
+router = DefaultRouter()
+router.register('tickets', TicketViewSet)
+urlpatterns = router.urls
+
+
