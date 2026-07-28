39a2eeeee998265ad83d0941d2a7a40b072de47e
rukigaki <rukigaki@gmail.com>
2026-07-28 18:35:59 +0300

    Create handler for the first test case

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -8,3 +8,11 @@ keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Cre
 
                                                  [InlineKeyboardButton(text="Patch", callback_data="patch"),
                                                   InlineKeyboardButton(text="Put", callback_data="put")]])
+
+category_keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Бочок потик", callback_data="create_ticket_toilet_broke"),
+                                                           InlineKeyboardButton(text="Телевизор поломался", callback_data="create_ticket_tv_broke"),
+                                                           InlineKeyboardButton(text="Вернуться назад", callback_data="return_back")]])
+
+
+boolean_keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Yes", callback_data="pressed_yes"),
+                                                          InlineKeyboardButton(text="No", callback_data="pressed_no")]])
---
+++ bot/utils.py
@@ -0,0 +1,14 @@
+import httpx
+from aiogram.types import Message
+
+
+async def create_ticket(message: Message, title, desc="description"):
+    async with httpx.AsyncClient() as client:
+        await client.post(
+            "http://127.0.0.1:8000/api/tickets/",
+            json={
+                "title": title,
+                "description": desc
+            }
+        )
+    await message.answer("Тикет был успешно создан!")
